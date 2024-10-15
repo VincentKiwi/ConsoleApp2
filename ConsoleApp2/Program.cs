7308a9487de8820c45916778b18084f1732baa01
VincentKiwi <vincent.kiwanuka@elev.ga.ntig.se>
2024-10-15 10:35:02 +0200

    first

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,19 @@
 ﻿
-Console.WriteLine("hello come to tom land");
+Console.WriteLine("hello come to tama land");
+
+tamagotchi MyTama = new tamagotchi();
+
+Console.WriteLine($"what name do you want type+enter");
+MyTama.name = "Console.ReadLine()";
+
+Console.WriteLine("great {MyTama.name} is the best u could come up with");
+Console.WriteLine("enter");
+Console.ReadKey();
+
+while (MyTama.GetAlive() == true)
+{
+    Console.Clear();
+    
+}
+
 
--- ConsoleApp2/tom.cs
+++ ConsoleApp2/tom.cs
@@ -5,7 +5,24 @@ public class tamagotchi
 {
 private int hunger;
 private int boredom;
-private List<string> words = new List<string>() {"booonk"}
+private List<string> words = new List<string>() {"booonk"};
+private bool isAlive;
+public string name;
+
+public tamagotchi()
+{
+    isAlive = true;
+}
+
+public void Feed()
+{
+    Console.WriteLine($"[{name}] is fed, [{name}] is becoming more full ");
+    hunger -= 2;
+    if (hunger < 0)
+    {
+        hunger = 0;
+    }
+}
 
 
 
