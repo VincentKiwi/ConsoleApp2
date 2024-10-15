b59ef8c10f0b5a404d5aada926cc018aab6956b6
VincentKiwi <vincent.kiwanuka@elev.ga.ntig.se>
2024-10-15 10:12:56 +0200

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,3 @@
+﻿
+Console.WriteLine("hello come to tom land");
+
---
+++ ConsoleApp2/tom.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class tamagotchi
+{
+private int hunger;
+private int boredom;
+private List<string> words = new List<string>() {"booonk"}
+
+
+
+}
